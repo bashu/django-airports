f86c23c0cdd9d3defec90b807f3f05764c757299
bashu <basil.shubin@gmail.com>
2015-03-28 09:07:54 +0600

    bump version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-airports',
-    version='0.1',
+    version='0.1.1',
     packages=['airports'],
     include_package_data=True,
     license='MIT License',
