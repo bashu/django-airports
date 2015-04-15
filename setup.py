62550c6eeab1c133eaa22b3009ec7f73f9c5e4e8
bashu <basil.shubin@gmail.com>
2015-04-15 12:51:47 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-airports',
     version='0.1.1',
-    packages=['airports'],
+    packages=find_packages(exclude=['example']),
     include_package_data=True,
     license='MIT License',
     description='Airport model and worldwide airport data for Django',
