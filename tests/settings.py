28b30fdf1a93921db13f1ce19549a1d55cd3aec2
Antonio Ercole De Luca <eracle@posteo.eu>
2018-03-14 20:25:22 -0500

    pinning os in travis 11

--- tests/settings.py
+++ tests/settings.py
@@ -9,7 +9,7 @@ USE_TZ = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "!2*nm%ps%x8!ykyb^s9+!l1vcmeh+(f&de%br=js*7(5i_rmet"
 
-SPATIALITE_LIBRARY_PATH = 'mod_spatialite'
+#SPATIALITE_LIBRARY_PATH = 'mod_spatialite'
 DATABASES = {
     "default": {
         "ENGINE": 'django.contrib.gis.db.backends.spatialite',
