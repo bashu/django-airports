c5f47eb75645efc32fd6fb83873718bae95cf084
Antonio Ercole De Luca <eracle@posteo.eu>
2018-03-14 19:32:26 -0500

    fixed problem with travis 6

--- tests/settings.py
+++ tests/settings.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8
 from __future__ import unicode_literals, absolute_import
 
+import os
+
 import django
 
 DEBUG = True
@@ -9,7 +11,10 @@ USE_TZ = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "!2*nm%ps%x8!ykyb^s9+!l1vcmeh+(f&de%br=js*7(5i_rmet"
 
-SPATIALITE_LIBRARY_PATH = 'mod_spatialite'
+# Travis uses trusty which have no mod_spatialite
+if not os.environ.get('TRAVIS'):
+    SPATIALITE_LIBRARY_PATH = 'mod_spatialite'
+
 DATABASES = {
     "default": {
         "ENGINE": 'django.contrib.gis.db.backends.spatialite',
