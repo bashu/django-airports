7faa38bc852cb4cbd8e4fc03f63b5fe6a9787ef4
Antonio Ercole De Luca <eracle@posteo.eu>
2018-03-14 20:08:44 -0500

    pinning os in travis 9

--- tests/settings.py
+++ tests/settings.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8
 from __future__ import unicode_literals, absolute_import
 
-import os
-
 import django
 
 DEBUG = True
@@ -11,10 +9,7 @@ USE_TZ = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "!2*nm%ps%x8!ykyb^s9+!l1vcmeh+(f&de%br=js*7(5i_rmet"
 
-# Travis uses trusty which have no mod_spatialite
-if 'TRAVIS' in os.environ:
-    SPATIALITE_LIBRARY_PATH = 'mod_spatialite'
-
+SPATIALITE_LIBRARY_PATH = 'mod_spatialite'
 DATABASES = {
     "default": {
         "ENGINE": 'django.contrib.gis.db.backends.spatialite',
